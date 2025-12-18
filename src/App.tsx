b5c82adde6a71cc17dc0c11d5def57a6246fdede
Student Sciencespo <student@MacBook-Pro-de-Emmanuel-2.local>
2025-12-18 11:55:21 +0100

    fix: use HashRouter for S3 compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage/LandingPage'
 import EcomForm from './Pages/EcomForm/EcomForm'
 
